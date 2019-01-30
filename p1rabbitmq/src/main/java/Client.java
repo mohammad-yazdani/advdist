7f1a38c268d4920b24b7eaae1777516288e0ee1d
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2019-01-29 23:39:43 -0500

    P1 done

--- p1rabbitmq/src/main/java/Client.java
+++ p1rabbitmq/src/main/java/Client.java
@@ -3,20 +3,64 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import stats.DataSet;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Client {
 
     private final static String QUEUE_NAME = "q0";
 
-    public static void main(String[] argv) {
+    private static void large() {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        DataSet bench = new DataSet();
+        String largeFileName = s + "/src/main/resources/10mb.txt";
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("18.221.69.86");
         try {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
-
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
 
+            for (int i = 0; i < 100; i++) {
+                String buffer;
+                try(BufferedReader br = new BufferedReader(new FileReader(largeFileName))) {
+                    StringBuilder sb = new StringBuilder();
+                    String line = br.readLine();
+
+                    while (line != null) {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                        line = br.readLine();
+                    }
+                    buffer = sb.toString();
+                }
+
+                long start = System.nanoTime();
+                channel.basicPublish("", QUEUE_NAME, null, buffer.getBytes());
+                long end = System.nanoTime();
+                bench.addData(end - start);
+                System.out.println("ITERATION " + i);
+            }
+
+            bench.save("RabbitMQLarge");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void simple() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("18.221.69.86");
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            String message;
             DataSet bench = new DataSet();
             for (int i = 0; i < 100; i++) {
                 long start = System.nanoTime();
@@ -31,5 +75,10 @@ public class Client {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void main(String[] argv) {
+        Client.simple();
+        Client.large();
     }
 }
